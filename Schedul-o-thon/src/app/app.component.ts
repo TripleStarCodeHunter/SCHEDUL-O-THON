9a4134d71c484117e037af5f5348ef17ba33aa9e
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-11 11:17:20 +0530

    removing commit from main

--- Schedul-o-thon/src/app/app.component.ts
+++ Schedul-o-thon/src/app/app.component.ts
@@ -1,14 +1,9 @@
-/*
-  AppComponent is the topmost component by default.
-  Other components are created as a child of AppComponent.
-*/
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
   title = 'Schedul-o-thon';
