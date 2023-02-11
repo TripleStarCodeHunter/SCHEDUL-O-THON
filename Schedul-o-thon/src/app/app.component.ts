aecc669a3ab6832d3072fefd0b0b481063f0b362
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-11 14:57:30 +0530

    generated landing-page compoenent

--- Schedul-o-thon/src/app/app.component.ts
+++ Schedul-o-thon/src/app/app.component.ts
@@ -1,9 +1,14 @@
+/*
+  AppComponent is the topmost component by default.
+  Other components are created as a child of AppComponent.
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Schedul-o-thon';
