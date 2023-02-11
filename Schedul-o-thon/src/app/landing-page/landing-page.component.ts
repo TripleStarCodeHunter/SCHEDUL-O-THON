b0564be1b811f50501d2b377b90af90ddd1ff60e
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-11 17:09:15 +0530

    landing page

--- Schedul-o-thon/src/app/landing-page/landing-page.component.ts
+++ Schedul-o-thon/src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+declare var AOS:any;
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.sass']
+  styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
-
+export class LandingPageComponent implements OnInit{
+  ngOnInit() {
+      AOS.init();
+  }
 }
