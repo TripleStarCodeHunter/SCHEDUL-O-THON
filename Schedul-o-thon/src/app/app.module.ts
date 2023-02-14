17e0395571b0329d488d3dec55fdb23fca023d3b
Niharika Harmalkar <niharikaharmalkar@gmail.com>
2023-02-14 23:20:53 +0530

    front-end navbar changes done

--- Schedul-o-thon/src/app/app.module.ts
+++ Schedul-o-thon/src/app/app.module.ts
@@ -5,26 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-<<<<<<< HEAD
 
-
-=======
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> 74a6e541c6e83f22c121f1c893aa94c39e6c79d3
+// import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-<<<<<<< HEAD
-    
-    
-=======
+
     LoginPageComponent,
     RegisterPageComponent,
-    NavbarComponent
->>>>>>> 74a6e541c6e83f22c121f1c893aa94c39e6c79d3
+    // NavbarComponent
+
   ],
   imports: [
     BrowserModule,
