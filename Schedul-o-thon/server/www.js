a2602981d62d95779ec8a1be837f1b925df1ecd8
Bilal Malik <bilalmalik504@yahoo.com>
2023-02-15 15:41:52 +0530

    created register_info table

--- Schedul-o-thon/server/www.js
+++ Schedul-o-thon/server/www.js
@@ -28,7 +28,7 @@ app.post(`${rootUrl}/user`, (req, res) => {
 });
 
 /*A dummy request of the server fetching the data from database*/ 
-
+//We even have a register_info table
 app.get(`${rootUrl}/data`, (req, res) => {
   client.query("SELECT * FROM batch_info", (err, result) => {
     if (err) {
