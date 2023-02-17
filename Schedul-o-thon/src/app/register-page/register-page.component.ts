7ffa0b30784fb78b57eba7cc5c25670e1e0fb0e1
shri-1805 <shrinu.mano1803@gmail.com>
2023-02-17 19:50:42 +0530

    register-page default form validators added

--- Schedul-o-thon/src/app/register-page/register-page.component.ts
+++ Schedul-o-thon/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.scss']
 })
 export class RegisterPageComponent {
+    constructor(){ }
+    ngOnInit():void{
+    }
+    registration=new FormGroup({
+      fullname: new FormControl("",[Validators.required]),
+      username: new FormGroup("",[Validators.required]),
+      email: new FormControl("",[Validators.required]),
+      mobile:new FormControl("",[Validators.required]),
+      password:new FormControl("",[Validators.required]),
+      conf_password: new FormControl("",[Validators.required]) 
+    });
 
+    registerSubmitted(){
+        console.log(this.registration.get("fullname"));
+    }
+
+    get FullName():FormControl{
+        return this.registration.get("fullname") as FormControl;
+    }
+
+    get UserName():FormControl{
+      return this.registration.get("username") as FormControl;
+    }
+
+    get Email():FormControl{
+      return this.registration.get("email") as FormControl;
+    }
+
+    get Mobile():FormControl{
+      return this.registration.get("mobile") as FormControl;
+    }
+    
+    get Password():FormControl{
+      return this.registration.get("password") as FormControl;
+    }
+
+    get ConfirmPassword():FormControl{
+      return this.registration.get("conf_password") as FormControl;
+  }
 }
---
+++ Schedul-o-thon/src/app/register-page/register-page.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ]
+})
+export class RegisterPageModule { }
