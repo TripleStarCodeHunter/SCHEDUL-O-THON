275df0b71de05daf2a9eb79543670095c1686288
Gulshan <2sagulshan@gmail.com>
2023-03-16 00:34:00 +0530

    added sub btach name in section form

--- Schedul-o-thon/server/utils/routes.js
+++ Schedul-o-thon/server/utils/routes.js
@@ -323,12 +323,12 @@ router.post(`${rootUrl}/section`, async (req, res) => {
     classroom,
     section_dl,
     trainee_list,
-    sub_batch_name
+    subb
   } = req.body;
 
   console.log(req.body);
 
-  const new_sub_batch_name = sub_batch_name;
+  const new_sub_batch_name = subb;
   console.log(new_sub_batch_name)
 
 
@@ -337,7 +337,7 @@ router.post(`${rootUrl}/section`, async (req, res) => {
   let sqlq = "SELECT * FROM sections_info where section_name='" + sectionName + "'";
   let sub_batch_id;
   let get_subbatchid = "SELECT sub_batch_id FROM sub_batches WHERE sub_batch_name = '" + new_sub_batch_name + "'";
-  console.log(get_subbatchid)
+  // console.log(get_subbatchid)
   client.query(get_subbatchid, (err, result) => {
     if (err) {
       res.json({ message: err });
@@ -350,7 +350,7 @@ router.post(`${rootUrl}/section`, async (req, res) => {
   })
 
   client.query(sqlq, (err, result) => {
-    console.log(result)
+    // console.log(result)
     if (err) {
       res.json({ message: err });
     }
@@ -370,7 +370,7 @@ router.post(`${rootUrl}/section`, async (req, res) => {
           section_dl,
           trainee_list,
           sub_batch_id,
-          sub_batch_name,
+          subb,
 
         ],
       };
--- Schedul-o-thon/src/app/section-form/section-form.component.ts
+++ Schedul-o-thon/src/app/section-form/section-form.component.ts
@@ -1,4 +1,4 @@
-import { Component,ViewEncapsulation, OnInit, Inject  } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, Inject } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -7,14 +7,15 @@ import {
 } from '@angular/forms';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
+import { GetSectionService } from '../shared/services/get-section.service';
 
 @Component({
   selector: 'app-section-form',
   templateUrl: './section-form.component.html',
   styleUrls: ['./section-form.component.scss']
 })
-export class SectionFormComponent implements OnInit{
-  ngOnInit(): void {}
+export class SectionFormComponent implements OnInit {
+  // ngOnInit(): void {}
   isSubmitted = false;
   SectionOwner: any = ['Owner1', 'Owner2', 'Owner3'];
   Track: any = [
@@ -22,14 +23,15 @@ export class SectionFormComponent implements OnInit{
     'Big Data',
     'Python'
   ];
-  Subbatch:any=['S1','S2','S3'];
+  Subbatch: any = ['S1', 'S2', 'S3'];
 
   numberPattern = '^[0-9]{1,4}$';
   constructor(
     private fb: FormBuilder,
     @Inject(MatSnackBar) private _snackBar: MatSnackBar,
-    private http: HttpClient
-  ) {}
+    private http: HttpClient,
+    private GetSectionService: GetSectionService,
+  ) { }
   section = this.fb.group({
     sectionName: ['', Validators.required],
     strength: ['', Validators.required],
@@ -39,15 +41,26 @@ export class SectionFormComponent implements OnInit{
     classroom: ['', Validators.required],
     // schedule: ['', Validators.required],
     trainee_list: ['', Validators.required],
-    subb:['',Validators.required]
+    subb: ['', Validators.required]
   });
-  
+
+
+  data!: any[];
+
+  ngOnInit() {
+    this.GetSectionService.getData().subscribe((data) => {
+      this.data = data;
+      console.log(this.data)
+    });
+  }
+
+
   changeSubbatch(e: any) {
     this.Subbatch?.setValue(e.target.value, {
       onlySelf: true,
     });
   }
-  
+
   get sectionName() {
     return this.section.get('sectionName');
   }
@@ -70,6 +83,8 @@ export class SectionFormComponent implements OnInit{
   get section_owner() {
     return this.section.get('section_owner');
   }
+
+
   get section_dl() {
     return this.section.get('section_dl');
   }
@@ -82,6 +97,12 @@ export class SectionFormComponent implements OnInit{
   get trainee_list() {
     return this.section.get('trainee_list');
   }
+
+  get subb() {
+    return this.section.get('subb');
+  }
+
+
   durationInSeconds = 5;
   onSubmit() {
     this.isSubmitted = true;
@@ -98,9 +119,15 @@ export class SectionFormComponent implements OnInit{
         section_dl: this.section.value.section_dl,
         classroom: this.section.value.classroom,
         trainee_list: this.section.value.trainee_list,
+        subb: this.section.value.subb
 
       };
-      // console.log(formData);
+
+
+      this.http.get('http://localhost:3000/api/login').subscribe((response) => {
+        console.log(response);
+      });
+      console.log(formData);
       this.http
         .post('http://localhost:3000/api/section', formData)
         .subscribe((response) => {
