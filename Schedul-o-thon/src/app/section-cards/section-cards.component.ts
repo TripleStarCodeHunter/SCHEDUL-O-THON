2344d4436803add004f34f7a5ea9f461f1abf724
shri-1805 <shrinu.mano1803@gmail.com>
2023-04-05 20:12:51 +0530

    Corrected section cards

--- Schedul-o-thon/src/app/section-cards/section-cards.component.ts
+++ Schedul-o-thon/src/app/section-cards/section-cards.component.ts
@@ -9,9 +9,9 @@ import { ActivatedRoute } from '@angular/router';
 export class SectionCardsComponent {
   data !: any[];
   // d=[
-  //   {section_name:"2022 Aug Java A",strength:30,track_name:"Java",classroom_name:"Mysuru-Aug-2022-A",schedule:"link here",section_id:"A"},
-  //   {section_name:"2022 Aug Java B",strength:30,track_name:"Java",classroom_name:"Mysuru-Aug-2022-B",schedule:"link here",section_id:"B"},
-  //   {section_name:"2022 Aug Java A",strength:30,track_name:"Java",classroom_name:"Mysuru-Aug-2022-C",schedule:"link here",section_id:"C"},
+  //   {section_name:"2022 Aug Java A",strength:30,track:"Java",classroom:"Mysuru-Aug-2022-A",schedule:"link here",section_id:"A"},
+  //   {section_name:"2022 Aug Java B",strength:30,track:"Java",classroom:"Mysuru-Aug-2022-B",schedule:"link here",section_id:"B"},
+  //   {section_name:"2022 Aug Java A",strength:30,track:"Java",classroom:"Mysuru-Aug-2022-C",schedule:"link here",section_id:"C"},
   // ];
 
   constructor(private dataService: SectionDataService, private route:ActivatedRoute) { }
