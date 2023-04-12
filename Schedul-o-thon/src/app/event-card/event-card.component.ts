c8b62ad1a9dd560d28f34e7171ef0b43a57cc261
Bilal Malik <bilalmalik504@yahoo.com>
2023-04-12 21:50:37 +0530

    added snackbar to event deletion

--- Schedul-o-thon/src/app/event-card/event-card.component.ts
+++ Schedul-o-thon/src/app/event-card/event-card.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ChangeDetectorRef, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { EventService } from '../shared/services/event.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-event-card',
@@ -14,7 +15,9 @@ export class EventCardComponent {
   constructor(
     private dataService: EventService,
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    @Inject(MatSnackBar) private _snackBar: MatSnackBar,
+
   ) {}
 
   ngOnInit() {
@@ -22,7 +25,16 @@ export class EventCardComponent {
     this.dataService.getData(id).subscribe((data) => {
       this.data = data;
     });
-    console.log(this.data);
+    const snackbarData = localStorage.getItem('snackbarData');
+      if (snackbarData) {
+        // Display snackbar
+        this._snackBar.open(JSON.parse(snackbarData).message, JSON.parse(snackbarData).action, {
+          duration: JSON.parse(snackbarData).duration
+        });
+        
+        // Remove snackbar data from localStorage
+        localStorage.removeItem('snackbarData');
+      }
   }
 
   onDelete(id: number) {
@@ -30,7 +42,13 @@ export class EventCardComponent {
     this.http
       .delete(`http://localhost:3000/api/del-event/${id}`)
       .subscribe((response) => {
-        console.log(response);
+        this.data = this.data.filter(event => event.id !== id);
       });
+      window.location.reload();
+    localStorage.setItem('snackbarData', JSON.stringify({
+      message: 'Event Deleted',
+      action: 'OK',
+      duration: 5 * 1000 //seconds*1000
+    }));
   }
 }
